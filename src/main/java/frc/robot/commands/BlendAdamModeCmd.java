1de1376b5d4fafbd5746125ad272788254711915
bcrteammember <bcrteammember@gmail.com>
2026-03-10 18:41:30 -0400

    Attempt to fix blender mode (values need tuning)

--- src/main/java/frc/robot/commands/BlendAdamModeCmd.java
+++ src/main/java/frc/robot/commands/BlendAdamModeCmd.java
@@ -1,56 +1,36 @@
 package frc.robot.commands;
 
-import java.util.List;
-
-import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants;
 import frc.robot.ExtraMath;
-import frc.robot.LimelightHelpers;
 import frc.robot.RobotContainer;
 import frc.robot.drive.CommandSwerveDrivetrain;
 
 public class BlendAdamModeCmd extends Command {
-    private static enum BlenderWalls {
-        BLUE_HUB, WALL_LEFT_FROM_BLUE, RED_HUB, WALL_RIGHT_FROM_BLUE
-    };
 
     private final RobotContainer robot;
 
     private CommandSwerveDrivetrain drivetrain;
     private Pose2d currentPose2d;
 
-    private double MaxSpeed = RobotContainer.MaxSpeed;
-    private double MaxAngularRate = RobotContainer.MaxAngularRate;
+    // TODO correct these values for the errors on the blue side, and for a rotation buffer
 
-    private final SwerveRequest.FieldCentric driveFC = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1)
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-    private CommandXboxController m_driverController;
+    // in meters
+    private double blueHUBLine = 5.7;
+    // TODO FIND VALUE
+    private double redHUBLine = 11.6;
 
-    //in meters
-    private double blueHUBXLine = 5.7;
-
-    //TODO FIND VALUE
-    private double redHUBXLine = 11.6;
-
-    private double wallLFromBYLine = 7.590;
-    private double wallRFromBYLine = 0.319;
-
-
+    // left/right from blue's perspective
+    private double wallLeftLine = 7.590;
+    private double wallRightLine = 0.319;
 
     public BlendAdamModeCmd(RobotContainer robot) {
         this.drivetrain = robot.drivetrain;
-        this.m_driverController = robot.driverController;
         this.robot = robot;
     }
 
@@ -62,81 +42,98 @@ public class BlendAdamModeCmd extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if (true) {
-            currentPose2d = drivetrain.getState().Pose;
-            var x = currentPose2d.getX();
-            var y = currentPose2d.getY();
 
-            /*
-             * If (in a corner, allow driver X and Y)
-             * If (On one of the X lines, only allow Y)
-             * If (On one of the Y lines, only allow X)
-             * If (Not on either, free reign)
-             */
+        /*
+         * If (in a corner, allow driver X and Y)
+         * If (On one of the X lines, only allow Y)
+         * If (On one of the Y lines, only allow X)
+         * If (Not on either, free reign)
+         */
 
-            if((ExtraMath.within(y, redHUBXLine, 0.5)
-                || ExtraMath.within(y, blueHUBXLine, 0.5)) &&
-                    (ExtraMath.within(x, wallLFromBYLine, 0.5)
-                || ExtraMath.within(x, wallRFromBYLine, 0.5))){
+        currentPose2d = drivetrain.getState().Pose;
+        var x = currentPose2d.getX();
+        var y = currentPose2d.getY();
 
-                    drivetrain.setControl(driveFC.withVelocityX(-m_driverController.getLeftY() * MaxSpeed)
-                    .withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
+        double epsilon = 0.1; // TODO: find good value for this
 
-                    SmartDashboard.putString("BlendState", "On Corner");
-            }
-            else if(ExtraMath.within(x, redHUBXLine, 0.5)
-                || ExtraMath.within(x, blueHUBXLine, 0.5)){
-                    ChassisSpeeds fieldSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(drivetrain.getState().Speeds, drivetrain.getState().Pose.getRotation());
+        // Red/blue lines are parallel to Y, compare to X
+        // Side lines are parallel to X, compare to Y
 
-                    double robotVelocityX = fieldSpeeds.vxMetersPerSecond;
-                    double robotVelocityY = fieldSpeeds.vyMetersPerSecond;
+        // wallSomethingLine is from blue's perspective
 
-                    if(ExtraMath.within(x, redHUBXLine, 0.5)){
-                        SmartDashboard.putString("BlendState", "On Red Hub Line");
-                        //TODO Is > and angle correct?
-                        if(robotVelocityY > 0){
-                            robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(0)).withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
-                        } else {
-                            robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(180)));
-                        }
-                    } else if (ExtraMath.within(x, blueHUBXLine, 0.5)){
-                        SmartDashboard.putString("BlendState", "On Blue Hub Line");
-                        if(robotVelocityY > 0){
-                            robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(180)).withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
-                        } else {
-                            robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(0)));
-                        }
-                    }
+        boolean onRedHubLine = ExtraMath.within(x, redHUBLine, epsilon);
+        boolean onBlueHubLine = ExtraMath.within(x, blueHUBLine, epsilon);
+        boolean onWallLFromBlueLine = ExtraMath.within(y, wallLeftLine, epsilon);
+        boolean onWallRFromBlueLine = ExtraMath.within(y, wallRightLine, epsilon);
 
-            }
-            else if(ExtraMath.within(y, wallLFromBYLine, 0.5)
-                || ExtraMath.within(y, wallRFromBYLine, 0.5)){
-                ChassisSpeeds fieldSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(drivetrain.getState().Speeds, drivetrain.getState().Pose.getRotation());
+        ChassisSpeeds fieldSpeeds = 
+            ChassisSpeeds.fromRobotRelativeSpeeds(drivetrain.getState().Speeds, drivetrain.getState().Pose.getRotation());
 
-                double robotVelocityX = fieldSpeeds.vxMetersPerSecond;
-                double robotVelocityY = fieldSpeeds.vyMetersPerSecond;
+        double robotVelocityX = fieldSpeeds.vxMetersPerSecond;
+        double robotVelocityY = fieldSpeeds.vyMetersPerSecond;
 
-                if(ExtraMath.within(y, wallRFromBYLine, 0.5)){
-                    SmartDashboard.putString("BlendState", "On Right From Blue Line");
-                    //TODO Is > and angle correct?
-                    if(robotVelocityX > 0){
-                        robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(90)).withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
-                    } else {
-                        robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(270)));
-                    }
-                } else if (ExtraMath.within(y, wallLFromBYLine, 0.5)){
-                    SmartDashboard.putString("BlendState", "On Left From Blue Line");
-                    if(robotVelocityX > 0){
-                        robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(270)).withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
-                    } else {
-                        robot.drivetrain.setControl(robot.driveFCFA.withTargetDirection(Rotation2d.fromDegrees(90)));
-                    }
-                }
+        if ((onRedHubLine || onBlueHubLine) && (onWallLFromBlueLine || onWallRFromBlueLine)) {
+
+            // we're in a corner
+            drivetrain.setControl(robot.driveFC
+                    .withVelocityX(-RobotContainer.driverY * RobotContainer.MaxSpeed)
+                    .withVelocityY(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                    .withRotationalRate(-RobotContainer.driverRot * RobotContainer.MaxAngularRate));
+            SmartDashboard.putString("BlendState", "On Corner");
+
+        } else if (onBlueHubLine || onRedHubLine) {
+
+            // on a hub line (parallel to Y)
+            if (robotVelocityY > 0) {
+                robot.drivetrain.setControl(robot.driveFCFA
+                        .withTargetDirection(Rotation2d.fromDegrees(90))
+                        .withVelocityY(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                );
             } else {
-                SmartDashboard.putString("BlendState", "Free Roam");
-                drivetrain.setControl(driveFC.withVelocityX(-m_driverController.getLeftY() * MaxSpeed)
-                    .withVelocityY(-m_driverController.getLeftX() * MaxSpeed));
-            } 
+                robot.drivetrain.setControl(robot.driveFCFA
+                        .withTargetDirection(Rotation2d.fromDegrees(-90))
+                        .withVelocityY(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                );
+            }
+
+            if (onRedHubLine) {
+                SmartDashboard.putString("BlendState", "On Red Hub Line");
+            } else if (onBlueHubLine) {
+                SmartDashboard.putString("BlendState", "On Blue Hub Line");
+            }
+
+        } else if (onWallLFromBlueLine || onWallRFromBlueLine) {
+        
+            // on a left/right wall line (parallel to X)
+            // TODO Is > and angle correct?
+            if (robotVelocityX > 0) {
+                robot.drivetrain.setControl(robot.driveFCFA
+                        .withTargetDirection(Rotation2d.fromDegrees(0))
+                        .withVelocityX(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                );
+            } else {
+                robot.drivetrain.setControl(robot.driveFCFA
+                        .withTargetDirection(Rotation2d.fromDegrees(180))
+                        .withVelocityX(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                );
+            }
+
+            if (onWallRFromBlueLine) {
+                SmartDashboard.putString("BlendState", "On Right From Blue Line");
+            } else if (onWallLFromBlueLine) {
+                SmartDashboard.putString("BlendState", "On Left From Blue Line");
+            }
+
+        } else {
+
+            // somewhere in the middle or in an alliance
+            SmartDashboard.putString("BlendState", "Free Roam");
+            drivetrain.setControl(robot.driveFC
+                    .withVelocityX(-RobotContainer.driverY * RobotContainer.MaxSpeed)
+                    .withVelocityY(-RobotContainer.driverX * RobotContainer.MaxSpeed)
+                    .withRotationalRate(-RobotContainer.driverRot * RobotContainer.MaxAngularRate)
+            );
+
         }
     }
 
